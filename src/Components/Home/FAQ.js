d36cc0b6c719bebc6b028ed3a04a5427d873e80c
Ashley Chu <ashleymchu@yahoo.com>
2021-07-26 09:40:31 -0400

    last changes ok

--- src/Components/Home/FAQ.js
+++ src/Components/Home/FAQ.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./FAQ.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function FAQ() {
-
-  const dataQ = [{"zero": ["How are you?", "Good."]}]
-  const listItems = dataQ.map((d) => <span class="">{d.zero[0]} {d.zero[1]}</span>);
 
     return (
         <div id="faqs" className="section">
@@ -14,13 +10,7 @@ function FAQ() {
             <h2 id="fourthTitle" className="headerTitle">Take the quiz! Find the meal for you:</h2>
             <div>
               <div class="container">
-                <div class="row">
-                  <div class="card-deck">
-                    <span class="col border-light">
-                      {listItems}
-                    </span>
-                  </div>
-                </div>
+                <iframe id="interactApp60feb8ac30e03900170f3ad6" width="800" height="800" allowTransparency="true" frameborder="0" src={"https://quiz.tryinteract.com/#/60feb8ac30e03900170f3ad6?method=iframe"}></iframe>
               </div>
             </div>
             </div>
