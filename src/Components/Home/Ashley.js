912cfbd70477028eeabb9795c2b878da3df0e860
Ashley Chu <ashleymchu@yahoo.com>
2021-08-11 14:49:06 -0400

    adding profile info

--- src/Components/Home/Ashley.js
+++ src/Components/Home/Ashley.js
@@ -9,7 +9,7 @@ function Ashley() {
         <img id="ashleyimg" src={groupImage}/>
         <div id="ashleymission" className="text">
             <MiniHeader title="ashley" class="mini-about" />
-            <h1>Meet Ashley!</h1>
+            <h1>Meet the developer!</h1>
             <h2>Hi, my name's Ashley and I'm currently a junior studying Electrical Engineering & Computer Science with a minor in Art History. </h2>
             <p>I'm a major foodie, animation enthusiast, and dog fanatic. When I'm not debugging projects, I'm heavily involved in the women in engineering space on campus including SWE, AWE, and CSK since they all made an incredible impact on me when I've needed them most.</p>
         </div>
